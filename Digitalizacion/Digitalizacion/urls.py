3b51124cba892c8d214599f6cc3b49c1a8dac63c
Francisco <pakitovand31@gmail.com>
2020-10-13 20:25:53 -0500

    Login

--- Digitalizacion/Digitalizacion/urls.py
+++ Digitalizacion/Digitalizacion/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('login/', include('login.urls')),
 ]
 
 """

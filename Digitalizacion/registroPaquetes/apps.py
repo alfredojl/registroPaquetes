e16b6c411bd80abdfb1f985ed78c2bcc6921da06
alfredojl <alfredo.jl323@gmail.com>
2020-10-14 09:10:24 -0500

    .

--- Digitalizacion/registroPaquetes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegistropaquetesConfig(AppConfig):
-    name = 'registroPaquetes'
--- Digitalizacion/registroPaquetes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='registroPaquetes'),
-    path('verificaDatos/', views.verificaDatos, name='verificaDatos')
-]
